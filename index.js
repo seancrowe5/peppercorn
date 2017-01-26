d7d368bbcc0ce8f6e0c056166fd84ecb575b2022
seancrowe5 <sean.crowe@me.com>
2017-01-26 12:28:40 -0600

    Received postbackl

--- index.js
+++ index.js
@@ -77,6 +77,8 @@ app.post('/webhook', function (req, res) {
    // you've successfully received the callback. Otherwise, the request
    // will time out and we will keep trying to resend.
    res.sendStatus(200);
+ }else if (event.postback) {
+          receivedPostback(event);
  }
 });
 
@@ -170,6 +172,23 @@ function sendTextMessage(recipientId, messageText) {
   };
 
   callSendAPI(messageData);
+}
+
+function receivedPostback(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfPostback = event.timestamp;
+
+  // The 'payload' param is a developer-defined field which is set in a postback 
+  // button for Structured Messages. 
+  var payload = event.postback.payload;
+
+  console.log("Received postback for user %d and page %d with payload '%s' " + 
+    "at %d", senderID, recipientID, payload, timeOfPostback);
+
+  // When a postback is called, we'll send a message back to the sender to 
+  // let them know it was successful
+  sendTextMessage(senderID, "Postback called");
 }
 
 
